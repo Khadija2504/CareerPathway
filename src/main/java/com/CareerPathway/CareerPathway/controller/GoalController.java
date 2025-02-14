91a5e7f29da9a4903a3c12309a0c92757eec95b9
Khadija2504 <khadijalady25@gmail.com>
2025-02-14 11:28:08 +0100

    CPW-29 update the security config and the goal controller

--- src/main/java/com/CareerPathway/CareerPathway/controller/GoalController.java
+++ src/main/java/com/CareerPathway/CareerPathway/controller/GoalController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/goal")
+@RequestMapping("/api/employee/goal")
 public class GoalController {
     @Autowired
     private GoalService goalService;
