b49de1c6dd3095aace93690fe1db17cda8b6621f
Khadija2504 <khadijalady25@gmail.com>
2025-02-17 02:39:37 +0100

    CPW-1 display all of the employee skill assessments

--- src/main/java/com/CareerPathway/CareerPathway/service/SkillService.java
+++ src/main/java/com/CareerPathway/CareerPathway/service/SkillService.java
@@ -1,9 +1,12 @@
 package com.CareerPathway.CareerPathway.service;
 
+import com.CareerPathway.CareerPathway.model.Employee;
 import com.CareerPathway.CareerPathway.model.Skill;
+import com.CareerPathway.CareerPathway.model.SkillAssessment;
 
 import java.util.List;
 
 public interface SkillService {
     List<Skill> getAllSkills();
+    List<SkillAssessment> getAllEmployeeSkillAssessments(Long userId);
 }
