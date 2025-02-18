79072cb303e4218067b6a3323da7b5e31d0445bf
Khadija2504 <khadijalady25@gmail.com>
2025-02-18 12:29:52 +0100

    CPW-14 CPW-20 update the messages between mentor and employee methods to get all of the messages

--- src/main/java/com/CareerPathway/CareerPathway/repository/MessageRepository.java
+++ src/main/java/com/CareerPathway/CareerPathway/repository/MessageRepository.java
@@ -2,9 +2,13 @@ package com.CareerPathway.CareerPathway.repository;
 
 import com.CareerPathway.CareerPathway.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
+
+    @Query("SELECT m FROM Message m WHERE (m.sender.id = :senderId AND m.receiver.id = :receiverId) OR (m.sender.id = :receiverId AND m.receiver.id = :senderId)")
+    List<Message> findMessagesBetweenUsers(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
 }
