4b8c663b52123bb7f2585df60d6d4478f3148fdb
Khadija2504 <khadijalady25@gmail.com>
2025-03-20 23:10:28 +0000

    Fix test configuration for PostgreSQL

--- src/test/java/com/CareerPathway/CareerPathway/CareerPathwayApplicationTests.java
+++ src/test/java/com/CareerPathway/CareerPathway/CareerPathwayApplicationTests.java
@@ -2,8 +2,10 @@ package com.CareerPathway.CareerPathway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CareerPathwayApplicationTests {
 
 	@Test
